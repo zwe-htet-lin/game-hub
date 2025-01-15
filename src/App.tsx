9ae24adee9d6ac2d9b4e19a09efb0f34d28b85cb
Zwe Htet Lin <zwehtetlin1222002@gmail.com>
2025-01-16 00:28:14 +0630

    build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { Button } from "./components/ui/button"
-
 function App() {
   return(
     <>
-      <div>Hello World</div>
-      <Button>Button</Button>
+      <div className="grid grid-cols-2 grid-rows-2">
+        <div className="bg-slate-500 col-span-2">Nav</div>
+        <div className="bg-stone-500 hidden lg:block">Aside</div>
+        <div className="bg-sky-500 col-span-2 lg:col-span-1">Main</div>
+      </div>
     </>
   )
 }
