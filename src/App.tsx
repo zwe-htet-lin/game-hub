040661895348389f9a4b9fd89a62bf6133c15ace
Zwe Htet Lin <zwehtetlin1222002@gmail.com>
2025-01-15 23:37:48 +0630

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+  return(
+    <>
+      <div>Hello World</div>
+      <Button>Button</Button>
+    </>
+  )
+}
+
+export default App
